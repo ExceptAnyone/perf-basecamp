f7baa3266c3e0ca1e91eb4d07afe4830fda2e176
장정안 <wjddks96@naver.com>
2025-09-03 16:47:45 +0900

    Updates

--- bundle.js
+++ bundle.js
@@ -3848,7 +3848,7 @@ var gifAPIService_awaiter = (undefined && undefined.__awaiter) || function (this
     });
 };
 
-const API_KEY = "MISSING_ENV_VAR".GIPHY_API_KEY;
+const API_KEY = "i1aTcMjaR8eRsIdQjg2Kmmv3kCuZVoND";
 if (!API_KEY) {
     throw new Error('GIPHY_API_KEY is not set in environment variables');
 }
